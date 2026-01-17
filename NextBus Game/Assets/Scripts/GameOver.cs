d59f593bef927bb0122af91bf35157db41fbe67d
Pavithra Achar <pavithra.achar1505@gmail.com>
2026-01-17 13:41:00 +0800

    Game Over

--- NextBus Game/Assets/Scripts/GameOver.cs
+++ NextBus Game/Assets/Scripts/GameOver.cs
@@ -1,16 +1,30 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class GameOver : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private bool isGameOver = false;
+
+    void OnCollisionEnter2D(Collision2D collision)
     {
-        
+        if (isGameOver) return;
+
+        if (collision.gameObject.CompareTag("Obstacle"))
+        {
+            TriggerGameOver();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    void TriggerGameOver()
     {
-        
+        isGameOver = true;
+
+        FindFirstObjectByType<GameOverUI>()?.Show();
+
+        // Stop time
+        Time.timeScale = 0f;
+
+        // Optional: disable player input
+        GetComponent<BusMovement>().enabled = false;
     }
 }
