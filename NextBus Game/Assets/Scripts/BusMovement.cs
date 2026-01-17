9b974f8b9ddef3753b8b3b5e527123f2e29d62d1
nichajasmine <nicha.6627@gmail.com>
2026-01-17 20:28:44 +0800

    by jas lvl5

--- NextBus Game/Assets/Scripts/BusMovement.cs
+++ NextBus Game/Assets/Scripts/BusMovement.cs
@@ -3,19 +3,35 @@ using UnityEngine.InputSystem;
 
 public class BusMovement : MonoBehaviour
 {
-    [SerializeField] private float speed = 5f;
+    [SerializeField] private float speed = 50f;
     [SerializeField] private Rigidbody2D front;
     [SerializeField] private Rigidbody2D back;
 
-private float _moveInput;
-    private void FixedUpdate()
+    private float _moveInput;
+    private float keyhelddown = 0f;
+
+    private void Update()
     {
-        front.AddTorque(-_moveInput * speed * Time.fixedDeltaTime);
-        back.AddTorque(-_moveInput * speed * Time.fixedDeltaTime);
+        if (Keyboard.current.downArrowKey.isPressed)
+        {
+            _moveInput = -1f;   
+            if (keyhelddown < 5f)
+                keyhelddown += Time.deltaTime;
+        } else if (Keyboard.current.upArrowKey.isPressed)
+        {
+            _moveInput = 1f;
+            if (keyhelddown < 5f)
+            keyhelddown += Time.deltaTime;
+        } else
+        {
+            _moveInput = 0f;
+            keyhelddown = 0f;
+        }   
     }
 
-    private void Update() {
-        _moveInput += Keyboard.current.leftArrowKey.isPressed ? -1f :
-                     Keyboard.current.rightArrowKey.isPressed ? 1f : 0f;
+    private void FixedUpdate()
+    {
+        front.AddTorque(-_moveInput * speed * keyhelddown);
+        back.AddTorque(-_moveInput * speed * keyhelddown);
     }
 }
